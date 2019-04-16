799d35f68a45158b777bd7c367cb0a01a98b9a48
BenM96 <1996bmercer@gmail.com>
2019-04-16 14:33:36 +0100

    add sysout test

--- src/test/java/accounts/accounts/ServiceTest.java
+++ src/test/java/accounts/accounts/ServiceTest.java
@@ -2,6 +2,8 @@ package accounts.accounts;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,6 +11,17 @@ import org.junit.*;
 
 public class ServiceTest {
 	static Map<Integer, Account> map= new HashMap<Integer, Account>();	 	
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;	
+	
+	
+	
+	
+	
+	
 	
 	@BeforeClass
 	public static void setup() {		
@@ -21,15 +34,55 @@ public class ServiceTest {
 	    map.put(account.getAccountNumber(), account);
 	    account = new Account("frank", "poo",4);
 	    map.put(account.getAccountNumber(), account);
+	    
+	    
 	}
 	
 	
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void restoreStreams() {
+	    System.setOut(originalOut);
+	    System.setErr(originalErr);
+	}
+	
+	
+	
 	@Test
-	public void test1() {
+	public void out() {
+	    System.out.print("hello");
+	    assertEquals("hello", outContent.toString());
+	}
+	
+	
+	
+	
+	
+	
+	@Test
+	public void test1() {		
+		
+		
 		Service service=new Service();
 		service.findName("john",map);
 		assertEquals(2,service.findName("john",map));
 		assertEquals(1,service.findName("dave", map));
+		assertEquals(0,service.findName("", map));
+		
+	}
+	
+	
+	
+	@Test
+	public void test2() {
+		Service service=new Service();
+		service.findName("john",map);
+		assertEquals(2,outContent.toString());
 	}
 	
 
